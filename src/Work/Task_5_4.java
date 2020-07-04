f4c1812cc199dcd31135a71557b399b97946bb55
Alexandr Kas <joedassin2015@gmail.com>
2020-07-04 14:09:54 +0300

    Задание 4(с добавлением) к теме 5.

--- src/Work/Task_5_4.java
+++ src/Work/Task_5_4.java
@@ -1,5 +1,7 @@
 package Work;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +11,9 @@ public class Task_5_4 {
 
         String str = "разделяем строку на слова на строку разделяем на";
         String[] arrayStr = str.split("\\s");
+       // String s = Arrays.toString(arrayStr);
 
-
-        Map<String, Integer> mp = new HashMap<String, Integer>();
+       /* Map<String, Integer> mp = new HashMap<String, Integer>();
         int count = 0;
 
         for (int i = 0; i < arrayStr.length; i++) {
@@ -24,6 +26,22 @@ public class Task_5_4 {
             mp.put(arrayStr[i], count);
         }
         System.out.println(mp);
+
+        */
+
+
+        int count = 0;
+        for (int i = 0; i < arrayStr.length; i++) {
+            count = 0;
+            for (int j = 0; j < arrayStr.length; j++) {
+                if (arrayStr[i].equalsIgnoreCase(arrayStr[j])){
+                    count++;
+                }
+            }
+           System.out.println(arrayStr[i] + " " + count);
+        }
+
+
 
     }
 }
