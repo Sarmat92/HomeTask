421534994c71015b3af0e43ac401fdb243110140
Alexandr Kas <joedassin2015@gmail.com>
2020-07-28 17:07:51 +0300

    домашка 11.

---
+++ src/Work/Homework_11.java
@@ -0,0 +1,40 @@
+package Work;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Homework_11 {
+
+    static Scanner scan = new Scanner(System.in);
+
+    private static int calc(int firstNum, int secondNum) {
+        return firstNum / secondNum;
+    }
+
+    public static void main(String[] args) {
+
+        boolean repeat = true;
+
+        do {
+
+            try {
+
+                int firstNum = scan.nextInt();
+                int secondNum = scan.nextInt();
+
+                System.out.println(calc(firstNum, secondNum));
+
+                repeat = false;
+
+            } catch (ArithmeticException e) {
+                System.out.println("Нельзя делить на ноль, лох!" + '\n' + e);
+
+            } catch (InputMismatchException e) {
+                System.out.println("Вводи только цифры, рукожоп!" + '\n' + e);
+            } finally {
+                System.out.println("Всегда будет выполняться!");
+            }
+        }while (repeat);
+    }
+
+}
